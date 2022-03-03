fba88d5551ba1952efa14c4e899a56ee4ad05096
EternityOlleto <95216965+EternityOlleto@users.noreply.github.com>
2022-03-03 21:44:14 +0300

    Add files via upload

---
+++ Курсовая работа/Курсовая работа/Form2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Курсовая_работа
+{
+    public partial class Form2 : Form
+    {
+        public SqlConnection conn = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\User\\Desktop\\курсач\\Курсовая работа\\Курсовая работа\\DB.mdf;Integrated Security=True");
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void orderBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.orderBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.dBDataSet);
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            this.orderTableAdapter4.Fill(this.dBDataSet4.Order);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+
+            string query = $"INSERT INTO [Order] (clientName, goodsType, goodsTransport, date, price) VALUES (@clientName, @goodsType, @goodsTransport, @date, @price)";
+            SqlCommand com = new SqlCommand(query, conn);
+
+            com.Parameters.Add("@clientName", System.Data.SqlDbType.NVarChar);
+            com.Parameters.Add("@goodsType", System.Data.SqlDbType.NVarChar);
+            com.Parameters.Add("@goodsTransport", System.Data.SqlDbType.NVarChar);
+            com.Parameters.Add("@date", System.Data.SqlDbType.Date);
+            com.Parameters.Add("@price", System.Data.SqlDbType.Decimal);
+
+            com.Parameters["@clientName"].Value = clientNameTextBox.Text;
+            com.Parameters["@goodsType"].Value = goodsTypeTextBox.Text;
+            com.Parameters["@goodsTransport"].Value = goodsObjectTextBox.Text;
+            com.Parameters["@date"].Value = dateDateTimePicker.Value;
+            com.Parameters["@price"].Value = Convert.ToDecimal(priceTextBox.Text);
+
+            com.ExecuteNonQuery();
+
+            conn.Close();
+        }
+    }
+}
---
+++ Курсовая работа/Курсовая работа/Generation.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Курсовая_работа
+{
+    class Generation
+    {
+        public TextBox tb;
+        private bool _isActive = false;
+        public SqlConnection conn = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\курсач\\Курсовая работа\\Курсовая работа\\DB.mdf;Integrated Security=True");
+        public Generation(TextBox t)                   
+        {
+
+            tb = t;
+            conn.Open();
+        }
+        public void Start()
+        {
+            _isActive = true;
+            Task.Run(() => Generate());
+            Task.Run(() => GenerateClients());
+            Task.Run(() => GenerateGoods());
+        }
+        public void Stop()
+        {
+            _isActive = false;
+        }
+        private void Generate()
+        {
+            while (_isActive)
+            {
+                Client c = PickClient();
+                Order order = GenerateOrder(c);
+                Checkout(order);
+                Thread.Sleep(1000);
+            }
+        }
+        public List<Client> clients = new List<Client>();
+        public List<Order> orders = new List<Order>();
+        public List<Goods> goods = new List<Goods>();
+        public Buy buy = new Buy();
+
+        public List<string> clientNames = new List<string> { "Покупатель"};
+        public List<string> goodsTypes = new List<string> { "отечетсвенный", "импортный"};
+        public List<string> goodsTransport = new List<string> { "морской", "сухопутный"};
+
+
+        public void GenerateClients()
+        {
+            while (_isActive)
+            {
+                Client c = new Client();
+                Random random = new Random();
+                c.Name = clientNames[random.Next(0, clientNames.Count)];
+                clients.Add(c);
+                Thread.Sleep(1000);
+            }
+        }
+        public void GenerateGoods()
+        {
+            while (_isActive)
+            {
+                Goods g = new Goods();
+                Random random = new Random();
+                g.Name = goodsTransport[random.Next(0, goodsTypes.Count)];
+                g.Object = goodsTypes[random.Next(0, goodsTransport.Count)];
+                if (g.Name == "морской") g.Price = random.Next(1, 9999) + 10000;
+                if (g.Name == "сухопутный") g.Price = random.Next(1, 9999) + 10000;
+                goods.Add(g);
+                Thread.Sleep(1000);
+            }
+        }
+        public Client PickClient()
+        {
+            if (clients.Count > 0)
+            {
+                Random random = new Random();
+                return clients[random.Next(0, clients.Count)];
+            }
+            return null;
+        }
+
+        public Order GenerateOrder(Client c)
+        {
+            if (c == null) return null;
+            if (goods.Count <= 0) return null;
+            Random random = new Random();
+            Goods g = goods[random.Next(0, goods.Count)];
+            Order order = new Order() { Client = c, Goods = g, Date = DateTime.Now };
+            return order;
+        }
+
+        public void Checkout(Order o)
+        {
+            if (o == null) return;
+            orders.Add(o);
+            buy.Account += o.Goods.Price;
+            tb.Invoke(new Action(() => tb.Text = $"{o.Client.Name} купил {o.Goods.Name} товар за {o.Goods.Price} руб. Тип перевозки: {o.Goods.Object}.  Дата заказа: {o.Date.ToString("yyyy.MM.dd")}"));
+            Insert(o);
+            Thread.Sleep(500);
+
+        }
+        public void Insert(Order o)
+        {
+            string query = "INSERT INTO [Order] (clientName, goodsType, goodsTransport, date, price) VALUES (@clientName, @goodsType, @goodsTransport, @date, @price)";
+            SqlCommand com = new SqlCommand(query, conn);
+            com.Parameters.Add("@clientName", System.Data.SqlDbType.NVarChar);
+            com.Parameters.Add("@goodsType", System.Data.SqlDbType.NVarChar);
+            com.Parameters.Add("@goodsTransport", System.Data.SqlDbType.NVarChar);
+            com.Parameters.Add("@date", System.Data.SqlDbType.Date);
+            com.Parameters.Add("@price", System.Data.SqlDbType.Decimal);
+            com.Parameters["@clientName"].Value = o.Client.Name;
+            com.Parameters["@goodsType"].Value = o.Goods.Name;
+            com.Parameters["@goodsTransport"].Value = o.Goods.Object;
+            com.Parameters["@date"].Value = o.Date;
+            com.Parameters["@price"].Value = o.Goods.Price;
+            com.ExecuteNonQuery();
+        }
+    }
+}
